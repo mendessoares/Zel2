6d360838dcdd3d0544ac075ceea96bb9abe4d3b4
mendessoares <m147094@r5016886.mayo.edu>
2015-08-09 14:23:29 -0500

    I think it will be useful to actually have an object with several characteristics of the community that will be used for the analysis along the way but also updated with each step. This will make it easier to jsut have everything concerning each community summarized in only one entry for instance.

---
+++ createCommunityObject.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+'''
+TODO:
+    Actually write the script
+    Insert error dealing
+'''
+
+'''
+NAME and SYNOPSIS 
+      createCommunityObject.py -- create community object with attributes such as species composition, average relatedness and others that will be added as I move along in the analysis.   
+'''
+
+'''
+DESCRIPTION
+    The script expects 
+'''
+
+'''
+EXAMPLES
+      Create a community object that will be used throughout the analysis.
+      > python createCommunityObject.py attribute1 attribute2 attribute3 etc
+      
+SEE ALSO
+      Uses output from scripts 
+
+AUTHORS
+      Helena Mendes-Soares - Mayo Clinic, Center for Individualized Medicine 
+'''
